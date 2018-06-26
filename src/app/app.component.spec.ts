d93c367e43bbe26438b0aba04a9bdb318106b47b
Prigovor.S <doroshenko.s.oranges@gmail.com>
2018-06-26 16:36:46 +0300

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {CurrentWeatherComponent} from './current-weather/current-weather.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        CurrentWeatherComponent
       ],
     }).compileComponents();
   }));
